bd99991f4fdcc194bcf119eeb7b66a7c4054e8da
Ph70x7 <paulo.heitor70x7@gmail.com>
2025-10-14 08:49:25 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -x 
+
 PORT=${PORT:-80}
 
 sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -i "s/:80/:${PORT}/" /etc/apache2/sites-available/000-default.conf
+
+echo "Starting Apache on port $PORT"
 
 apache2-foreground
